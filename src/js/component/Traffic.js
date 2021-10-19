1a1886ccfa755ba3f0032230afbc7ba6f1d50d26
Joel Santiago <jsant288@fiu.edu>
2021-10-19 01:51:02 +0000

    created traffic light component

---
+++ src/js/component/Traffic.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+//create your first component
+const Traffic = () => {
+	const [toggle, setToggle] = useState(true);
+
+	return (
+		<div className="text-center mt-5">
+			<button onClick={() => setToggle(!toggle)}>
+				{toggle ? "red" : "off"}
+			</button>
+
+			<div> Green Light </div>
+			<div> Yellow Light </div>
+		</div>
+	);
+};
+
+export default Traffic;
