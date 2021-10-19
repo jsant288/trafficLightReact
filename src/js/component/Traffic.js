6a5c08ed6e9408ac8a1bfca38c297809e057d65d
Joel Santiago <jsant288@fiu.edu>
2021-10-19 03:26:19 +0000

    added colors to components

--- src/js/component/Traffic.js
+++ src/js/component/Traffic.js
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 
 //create your first component
 const Traffic = () => {
-	const [toggle, setToggle] = useState(true);
+	const [toggle, setToggle] = useState("off");
+	let red = "bg-danger";
+	let green = "bg-success d-flex";
+	let yellow = "bg-warning";
+	let off = "bg-dark";
 
 	return (
 		<div className="text-center mt-5">
-			<button onClick={() => setToggle(!toggle)}>
-				{toggle ? "red" : "off"}
-			</button>
-
-			<div> Green Light </div>
-			<div> Yellow Light </div>
+			<div>
+				<button onClick={() => setToggle("green")}>
+					{toggle == "green" ? (
+						<div className={green}>on</div>
+					) : (
+						<div className={off}>off</div>
+					)}
+				</button>
+			</div>
+			<div>
+				<button className={yellow} onClick={() => setToggle("yellow")}>
+					{toggle == "green" || toggle == "red" ? "off" : "on"}
+				</button>
+			</div>
+			<div>
+				<button className={red} onClick={() => setToggle("red")}>
+					{toggle == "yellow" || toggle == "green" ? "off" : "on"}
+				</button>
+			</div>
 		</div>
 	);
 };
-
 export default Traffic;
