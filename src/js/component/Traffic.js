9eed59803d3e08f81d4943ae082801fb33f9a939
Joel Santiago <jsant288@fiu.edu>
2021-10-25 22:52:17 +0000

    rearranged the lights

--- src/js/component/Traffic.js
+++ src/js/component/Traffic.js
@@ -7,10 +7,11 @@ const Traffic = () => {
 	let green = "bg-success d-flex";
 	let yellow = "bg-warning";
 	let off = "bg-dark";
+	let divStyle = "col rounded-circles";
 
 	return (
-		<div className="text-center mt-5">
-			<div>
+		<div className="text-center mx-auto w-50 mt-5 row">
+			<div className={divStyle}>
 				<button onClick={() => setToggle("green")}>
 					{toggle == "green" ? (
 						<div className={green}>on</div>
@@ -19,12 +20,12 @@ const Traffic = () => {
 					)}
 				</button>
 			</div>
-			<div>
+			<div className={divStyle}>
 				<button className={yellow} onClick={() => setToggle("yellow")}>
 					{toggle == "green" || toggle == "red" ? "off" : "on"}
 				</button>
 			</div>
-			<div>
+			<div className={divStyle}>
 				<button className={red} onClick={() => setToggle("red")}>
 					{toggle == "yellow" || toggle == "green" ? "off" : "on"}
 				</button>
